71066cbeb5f4172fe954f2fe0aebdc9e8eb8171f
matsushin11 <matsu1983@gmail.com>
2014-07-06 23:36:17 +0900

    Initial commit.

---
+++ bin/vending_machine.rb
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+
+# 自動販売機クラス
+class VendingMachine
+  ACCEPTABLE_MONEY = [10, 50, 100, 500, 1000]
+  attr_reader :stock, :total, :sales
+  def initialize
+    @total = 0
+    @sales = 0
+    @stock = Array.new(5, Drink.new(name: "cola", price: 120))
+  end
+
+  # コインを投入する
+  def input(coin)
+    if ACCEPTABLE_MONEY.include?(coin)
+      @total += coin
+      true
+    else
+      false
+    end
+  end
+
+  # 払い戻し操作を行う
+  def refund
+    pay_back = @total
+    @total = 0
+    pay_back
+  end
+
+  # ジュース購入判定を行う
+  def can_buy?(name)
+    i = @stock.find_index{|drink|
+      drink.name == name
+    }
+    cola = @stock[i]
+    if (cola.price <= @total)
+      i
+    else
+      false
+    end
+  end
+
+  # 購入
+  def purchase(name)
+    i = can_buy?(name)
+    if i
+      @sales += @stock[i].price
+      @total -= @stock[i].price
+      @stock.delete_at(i)
+    end
+  end
+end
+
+class Drink
+  attr_accessor :name, :price
+  def initialize(name: name, price: price)
+    @name = name
+    @price = price
+  end
+end
+
---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+require File.expand_path('../spec_helper', __FILE__)
+
+RSpec.describe VendingMachine do
+
+  let (:vending_machine) do
+    VendingMachine.new()
+  end
+
+  it "10円玉を投入できる" do
+    answer = true
+    expect(vending_machine.input(10)).to eq answer
+  end
+
+  it "50円玉を投入できる" do
+    answer = true
+    expect(vending_machine.input(50)).to eq answer
+  end
+
+  it "100円玉を投入できる" do
+    answer = true
+    expect(vending_machine.input(100)).to eq answer
+  end
+
+  it "5000円玉を投入できる" do
+    answer = true
+    expect(vending_machine.input(500)).to eq answer
+  end
+
+  it "1000円札を投入できる" do
+    answer = true
+    expect(vending_machine.input(1000)).to eq answer
+  end
+
+  it "連続で投入する" do
+    answer = true
+    expect(vending_machine.input(10)).to eq answer
+    expect(vending_machine.input(10)).to eq answer
+  end
+
+  it "投入金額の合計を取得できる" do
+    answer = 60
+    vending_machine.input(10)
+    vending_machine.input(50)
+    expect(vending_machine.total).to eq answer
+    coin = 100
+    vending_machine.input(coin)
+    expect(vending_machine.total).to eq answer + coin
+  end
+
+  it "払い戻し操作ができる" do
+    answer = 60
+    vending_machine.input(10)
+    vending_machine.input(50)
+    expect(vending_machine.refund).to eq answer
+    expect(vending_machine.total).to eq 0
+  end
+
+  it "1円玉が投入されたら金額に加算しない" do
+    answer = 0
+    expect(vending_machine.input(1)).to eq false
+    expect(vending_machine.total).to eq answer
+  end
+
+  it "5円玉が投入されたら金額に加算しない" do
+    answer = 0
+    expect(vending_machine.input(5)).to eq false
+    expect(vending_machine.total).to eq answer
+  end
+
+  it "2000円冊が投入されたら金額に加算しない" do
+    answer = 0
+    expect(vending_machine.input(2000)).to eq false
+    expect(vending_machine.total).to eq answer
+  end
+
+  it "1本120円のコーラが5本入ってる" do
+    stock = vending_machine.stock
+    expect(stock.size).to eq 5
+    stock.each do |drink|
+      expect(drink.name).to eq 'cola'
+      expect(drink.price).to eq 120
+    end
+  end
+
+  it "コーラの購入ができるか判定を行う" do
+    vending_machine.input(100)
+    vending_machine.input(10)
+    expect(vending_machine.can_buy?('cola')).to eq false
+    vending_machine.input(10)
+    expect(!!vending_machine.can_buy?('cola')).to eq true
+
+  end
+
+  context "ジュースを購入する" do
+    it "ジュースを購入する" do
+      vending_machine.input(100)
+      vending_machine.input(50)
+      expect(vending_machine.purchase('cola')).to eq true
+
+
+    end
+
+    it "売り上げ金額を取得する" do
+      sales= 120
+      vending_machine.input(100)
+      vending_machine.input(50)
+      vending_machine.purchase('cola')
+      expect(vending_machine.sales).to eq sales
+    end
+
+    it "払い戻し操作をする" do
+      total = 30
+      vending_machine.input(100)
+      vending_machine.input(50)
+      vending_machine.purchase('cola')
+      expect(vending_machine.refund).to eq total
+    end
+  end
+end
